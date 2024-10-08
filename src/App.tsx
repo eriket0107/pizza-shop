f28d2b9843a4bdf8cfddcfae3b49e91529e33251
Erik <ol.erik0107@gmail.com>
2024-10-08 19:26:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+import "./global.css"
+
+
+export function App() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
