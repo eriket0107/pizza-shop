91fb8d87877eb7156fd684bb21242062546f2add
Erik <ol.erik0107@gmail.com>
2024-10-28 08:46:46 -0300

    feat: redirect unlogged user

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,31 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const inteceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data?.code
+          console.log({ status, code })
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            return navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    return () => api.interceptors.response.eject(inteceptorId)
+  }, [navigate])
+
   return (
     <main className="flex min-h-screen flex-col antialiased">
       <Header />
